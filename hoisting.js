b8e7097398b37f1c12adf52685928793cc9872a5
David Mendelovits <david.mendelovits@gmail.com>
2017-12-15 04:32:00 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -10,11 +10,10 @@ function callMe() {
 
 
 function crazy() {
-  var thisIsCrazy = function
-  {
+  var thisIsCrazy = function; {
     console.log("hey!!!")
   }
-  thisIsCrazy() 
+  thisIsCrazy()
 }
 
 function sayMyName() {
